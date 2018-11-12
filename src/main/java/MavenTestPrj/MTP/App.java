aef0baea78c82bcdd18a7b163f48ce5afb6e75af
Ivaturi <murthy.ivaturi@accenture.com>
2018-11-12 16:05:18 +0530

    Comit files to GIT HUB for Jenkins JOB

---
+++ src/main/java/MavenTestPrj/MTP/App.java
@@ -0,0 +1,13 @@
+package MavenTestPrj.MTP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
